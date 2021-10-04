f384d221fe7e38db95cd4a77aa3d1992cbcbdac4
DenisKotov18 <deniskotov203@gmail.com>
2021-10-04 23:29:54 +0300

    Added the output of the sum of the nubers of the numbers. Added the output of the factorial of the number. Added a number check on the divisors.

--- FindNumber/FindNumber/Source.cpp
+++ FindNumber/FindNumber/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include<ctime>
+#include <ctime>
+#include <cmath>
 using namespace std;
 int main()
 {
@@ -14,7 +15,7 @@ int main()
 	int number_hide;
 	number_hide = rand() % (b - a + 1) + a;
 	cout << " Якубович, крутите барабан" << endl << "Число загадно!" << endl << " ИГРА НАЧИНАЕТСЯ!" << endl;
-	int number;
+	int number = 0;
 	int gl;
 	gl = 0;
 	bool miss = false;
@@ -30,5 +31,35 @@ int main()
 		gl++;
 		miss = true;
 	} while (number != number_hide);
-	cout << "Вы победили! С " << gl << " попытки.";
+	cout << "Вы победили! С " << gl << " попытки." << endl;
+	int sum = 0;
+	int number_ost = number_hide;
+	do
+	{
+		sum += number_ost % 10;
+		number_ost /= 10;
+	} while (number_ost != 0);
+	cout << " Сумма цифр угаданного числа - "<< sum << endl;
+	int factorial = 1;
+	for (int i = 1; i <= number_hide; i++)
+	{
+		factorial *= i;
+	}
+	cout << " Факториал угаданного числа - " << factorial << endl;
+	bool trigger = false;
+	for (int i = 2; i <= sqrt(number_hide); i++)
+	{
+		if ((number_hide % i) == 0)
+		{
+			cout << " Делители угаданного числа - ";
+			for (int k = i; k <= (number_hide / 2); k++)
+			{
+				if ((number_hide % k) == 0) cout << k << " ";
+			}
+			trigger = true;
+			cout << endl;
+			break;
+		}
+	}
+	if (trigger == false) cout << " Угаданное число - простое";
 }
